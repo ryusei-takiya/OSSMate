0af4347aa815c7598b43f851b6db922fb7d26231
ryusei-takiya <afurochandesu@gmail.com>
2025-04-14 09:59:38 +0900

    フロントエンドにReact＋TypeScript導入

--- internal/interface/http/handler.go
+++ internal/interface/http/handler.go
@@ -2,6 +2,7 @@ package httpinterface
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/ryusei-takiya/ossmate/internal/usecase/fetch_trending"
@@ -9,9 +10,15 @@ import (
 
 func RegisterRoutes(router *gin.Engine) {
 	router.GET("/api/trending", func(c *gin.Context) {
+		// 各種パラメータを取得
 		language := c.Query("language")
-
-		repos, err := fetch_trending.FetchTrendingRepos(language)
+		pageStr := c.DefaultQuery("page", "1")
+		page, err := strconv.Atoi(pageStr)
+		if err != nil || page < 1 {
+			page = 1
+		}
+		// GithubAPI呼び出し
+		repos, err := fetch_trending.FetchTrendingRepos(language, page)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to fetch repositories"})
 			return
